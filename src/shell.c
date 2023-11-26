f56a7428cd3278f17620b01de47d7ff962688e7c
= <=>
2023-11-26 18:10:24 +0900

    shell 파이프라인 기능 추가

--- src/shell.c
+++ src/shell.c
@@ -10,6 +10,7 @@ void cmd_cd(int argc, char *argv[]);
 int getargs(char *cmd, char **argv);
 int execute_cmd(int narg, char *args[]);
 void redirect(int narg, char *argv[], int index);
+void pipeline(char *argv[], int narg, int index);
 
 int main() {
     char buf[256];
@@ -55,9 +56,11 @@ int getargs(char *cmd, char *argv[]) {
     argv[narg] = NULL;
     return narg;
 }
-void pipeline(char *argv[]) {
 
-}
+/*
+    명령줄에 특정 기호가 있는 지 확인하는 함수
+    기호의 인덱스를 index에 저장
+*/
 int check(char ch, char *argv[], int *index, int narg) {
     int i;
     for (i = 0; i < narg; i++) {
@@ -68,9 +71,7 @@ int check(char ch, char *argv[], int *index, int narg) {
     }
     return 0;
 }
-/*
-리다이렉트 명령이 있을 시 리다이렉트
-*/
+
 void redirect(int narg, char *argv[], int index){
     int fd;
     if (strcmp(argv[index], "<") == 0) {
@@ -80,24 +81,25 @@ void redirect(int narg, char *argv[], int index){
         }
         dup2(fd, STDIN_FILENO);
 
-        // 리다이렉트 기호와 파일 이름을 argv에서 삭제
+        // 리다이렉트 기호와 파일 이름을 argv에서 삭제 후 뒤 원소들을 앞으로 당김.
         memmove(argv + index, argv + index + 2, (narg - index - 1) * sizeof(char *));
         argv[narg] = NULL;
         close(fd);
     }
-    if (strcmp(argv[index], ">") == 0) {
+    else if (strcmp(argv[index], ">") == 0) {
         if ((fd = open(argv[index + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644)) == -1) {
             perror("open");
             exit(1);
         }
         dup2(fd, STDOUT_FILENO);
 
-        // 리다이렉트 기호와 파일 이름을 argv에서 삭제
+        // 리다이렉트 기호와 파일 이름을 argv에서 삭제 후 뒤 원소들을 앞으로 당김.
         memmove(argv + index, argv + index + 2, (narg - index - 1) * sizeof(char *));
         argv[narg] = NULL;
         close(fd);
     }
 }
+
 void cmd_cd(int argc, char *argv[]) {
     if (argc == 1) {
         argv[1] = getenv("HOME");
@@ -111,24 +113,56 @@ void cmd_cd(int argc, char *argv[]) {
         return;
     }
 }
-int execute_cmd(int narg, char *args[]) {
-    int status; // 전역으로?
-    int is_redirect = 0, is_pipe = 0, is_background = 0;
-    int index;
+
+void pipeline(char *argv[], int narg, int index) {
+    int p[2];
+    char *first_argv[50]; // 파이프 해야할 때 두 번째 명령어 저장
     pid_t pid;
 
-    is_redirect = check('<', args, &index, narg) || check('>', args, &index, narg);
-    is_pipe = check('|', args, &index, narg);
-    is_background = check('&', args, &index, narg);
+    memmove(first_argv, argv, index * sizeof(char *)); // 앞 명령어 new_arg에 저장
+    first_argv[index] = NULL;
+    memmove(argv, argv + index + 1, (narg - index - 1) * sizeof(char *)); // argv에 뒷 명령어만 남게 채움.
+    argv[narg - index - 1] = NULL;
+
+    if (pipe(p) == -1) {
+        perror("pipe call failed");
+        exit(1);
+    }
+
+    pid = fork();
+    if (pid == 0) {
+        dup2(p[1], STDOUT_FILENO);
+        execv(first_argv[0], first_argv);
+    }
+    else if (pid > 0) {
+        // close(p[1]);
+        dup2(p[0], STDIN_FILENO);
+    }
+    else {
+        perror("fork failed");
+        exit(1);
+    }
+}
+
+int execute_cmd(int narg, char *argv[]) {
+    int status; 
+    int is_redirect = 0, is_pipe = 0, is_background = 0;
+    int index[3];
+    pid_t pid;
+
+    is_redirect = check('<', argv, &index[0], narg) || check('>', argv, &index[0], narg);
+    is_pipe = check('|', argv, &index[1], narg);
+    is_background = check('&', argv, &index[2], narg);
 
     pid = fork();
     if (pid == 0) {
         if (is_pipe == 1) {
-
+            pipeline(argv, narg, index[1]);
         }
         if (is_redirect == 1) 
-            redirect(narg, args, index);
-        execv(args[0], args);
+            redirect(narg, argv, index[0]);
+        printf("%d", getpid());
+        execv(argv[0], argv);
         perror("exec");
         exit(1);
     }
